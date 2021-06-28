0e18358df471a8b1d8ba1cec004c0f6d888d11c4
Nikita Andrianov <inwide97@gmail.com>
2021-06-28 15:47:59 +0200

    Initial project

---
+++ app.js
@@ -0,0 +1,10 @@
+import cors from 'cors';
+import express from 'express';
+ 
+const app = express();
+ 
+app.use(cors());
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
