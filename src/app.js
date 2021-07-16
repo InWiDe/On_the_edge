a4f649ebaf5f440aadead0826409dda78f5defe2
Nikita Andrianov <inwide97@gmail.com>
2021-07-16 13:04:26 +0300

    Added several endpoints

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const engine = require('./services/stories/stories');
+const engine = require('./services/stories/classes');
 
 require ('./db/mongoose')
 
-// //User Router
-// const userProjectRouter = require('./services/user/user-project')
+//User Router
+const storyRouter = require('./services/stories/stories.js')
 
-// //Router for login/signup/
-// app.use(authRouter)
+//Router for stories
+app.use(storyRouter)
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
